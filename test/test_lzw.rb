df70e12a6e9c305d17eb8c9c22bcc71bf6eb8330
Pedro Alvarado <pwalvarado@gmail.com>
2015-06-09 14:01:04 -0500

    Adding basic tests

--- test/test_lzw.rb
+++ test/test_lzw.rb
@@ -1,5 +1,15 @@
 require_relative 'test_helper'
 require 'lzw'
 
-class LWZTest < MiniTest::Test
+class LZWTest < MiniTest::Test
+  def test_compress
+    compressed = LZW.compress('TOBEORNOTTOBEORTOBEORNOT')
+    assert_equal compressed, ["T", "O", "B", "E", "O", "R", "N", "O", "T", 256, 258, 260, 265, 259, 261, 263]
+  end
+
+  def test_decompress
+    string = 'TOBEORNOTTOBEORTOBEORNOT'
+    compressed = LZW.compress(string)
+    assert_equal LZW.decompress(compressed), string
+  end
 end
